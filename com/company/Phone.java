1e412f93c1d9b39d23e8931587d4511f0547baf0
Dasha M <dasha@example.com>
2018-05-16 20:30:37 +0300

    Методы и их вызов

--- com/company/Phone.java
+++ com/company/Phone.java
@@ -12,7 +12,7 @@ public class Phone {
     }
 
     Phone(int n, String m) {
-        this(4,5,"fdf");
+        this(4, 5, "fdf");
         n = 55;
         m = "kk";
     }
@@ -21,9 +21,21 @@ public class Phone {
 
     }
 
-    void receiveCall(String name) { System.out.println("Звонит "+ name); }
+    void receiveCall(String name, int num) {
+        System.out.println("Звонит " + name);
+    }
+
+    void receiveCall(String name) {
+        System.out.println("Звонит " + name + num);
+    }
 
     int getNumber() {
         return num;
     }
+
+    void sendMeassage(String... numbers) {
+        for (String n : numbers)
+            System.out.println("Sending message to phone " + n);
+    }
+
 }
--- com/company/PhoneDemo.java
+++ com/company/PhoneDemo.java
@@ -7,7 +7,11 @@ public class PhoneDemo {
         myPhone.receiveCall("Мама");
         myPhone.getNumber();
         System.out.println(myPhone.getNumber());
-        Phone myPhone2=new Phone(2,3,"jfjf");
-        Phone myPhone3=new Phone(44,"hfghfff");
+        /*Phone myPhone2=new Phone(2,3,"jfjf");
+        Phone myPhone3=new Phone(44,"hfghfff");*/
+        Phone call=new Phone();
+        call.receiveCall("Папа ", 757636);
+        Phone ms=new Phone();
+        ms.sendMeassage("5675757","7657474","565666656");
     }
 }
